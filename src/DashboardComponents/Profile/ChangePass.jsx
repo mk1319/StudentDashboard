8489a4a478fd2940610a06ee535c23926fbe18d8
Mukesh Rajpurohit <mukeshrajpurohit88977@gmail.com>
2020-11-27 20:44:53 +0530

    update password

--- src/DashboardComponents/Profile/ChangePass.jsx
+++ src/DashboardComponents/Profile/ChangePass.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import styled from "styled-components/macro";
-import axios from 'axios';
+import axios from "axios";
 import { connect } from "react-redux";
 
 const Container = styled.section`
   margin-top: 72px;
   min-width: 100%;
-  `
+`;
 const StyledForm = styled.form`
   display: grid;
   grid-template-columns: 3fr 3fr 1fr;
@@ -32,74 +32,66 @@ const StyledForm = styled.form`
   }
 `;
 
+function ChangePass({ ID }) {
+  const [Pass, setPass] = useState("");
+  const [Status, setStatus] = useState(false);
+  const [msg, setmsg] = useState("");
 
-function ChangePass({ID}){
-
-    const [Pass,setPass]=useState("")
-    const [Status,setStatus]=useState(false)
-    const [msg,setmsg]=useState("")
-
-    const handlesubmit=()=>{
-
-        if(Status)
-        {
-            axios.post('https://data.educationmandal.com/api/User/ChangePassword',{
-                ID:ID,
-                Password:Pass
-            })
-            .then((res)=>{
-                setmsg(res.data.msg)
-                setPass("")
-                setTimeout(()=>{
-                    setStatus(false)
-                    setmsg("")
-                },5000)
-            })
-
-        }
-        else
-        {
-            axios.post('https://data.educationmandal.com/api/User/CheckPassword',{
-                ID:ID,
-                Password:Pass
-            })
-            .then((res)=>{
-                setStatus(res.data.Status)
-                setmsg(res.data.msg)
-                setPass("")
-            })
-        }
+  const handlesubmit = () => {
+    if (Status) {
+      axios
+        .post("https://data.educationmandal.com/api/User/ChangePassword", {
+          ID: ID,
+          Password: Pass,
+        })
+        .then((res) => {
+          setmsg(res.data.msg);
+          setPass("");
+          setTimeout(() => {
+            setStatus(false);
+            setmsg("");
+          }, 5000);
+        });
+    } else {
+      axios
+        .post("https://data.educationmandal.com/api/User/CheckPassword", {
+          ID: ID,
+          Password: Pass,
+        })
+        .then((res) => {
+          setStatus(res.data.Status);
+          setmsg(res.data.msg);
+          setPass("");
+        });
     }
+  };
 
-    return(
+  return (
     <Container>
-        <h1>Enter {Status?"New":"Old"} Password:-</h1>
-        <h1 style={Status?{color:"Green"}:{color:"Red"}}>{msg}</h1>
-        <StyledForm
-            onSubmit={(e) => {
-                e.preventDefault();
-            }}
-        >
+      <h1>Enter {Status ? "New" : "Old"} Password:-</h1>
+      <h1 style={Status ? { color: "Green" } : { color: "Red" }}>{msg}</h1>
+      <StyledForm
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <input
           type="text"
           value={Pass}
-          onChange={(e)=>setPass(e.target.value)}
+          onChange={(e) => setPass(e.target.value)}
           required
-          placeholder={Status?"Enter New Password":"Enter Old Password"}
+          placeholder={Status ? "Enter New Password" : "Enter Old Password"}
         />
 
-        <button className="add" onClick={()=>handlesubmit()}>
+        <button className="add" onClick={() => handlesubmit()}>
           Enter
         </button>
         <label style={{ color: "green" }}>{}</label>
       </StyledForm>
     </Container>
-
-    )
-
+  );
 }
 
 export default connect((state) => ({
-    ID: state.Login.ID,
-  }))(ChangePass)
-  
+  ID: state.Login.ID,
+}))(ChangePass);
